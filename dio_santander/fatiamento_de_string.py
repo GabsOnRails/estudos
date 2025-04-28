be20514a49452e0b93014d176d4e5be843f2f361
Gabriel Felipe de Oliveira <167545171+GabsOnRails@users.noreply.github.com>
2025-04-28 13:19:57 -0300

    Delete dio_santander directory

--- dio_santander/fatiamento_de_string.py
+++
@@ -1,4 +0,0 @@
-nome = "Gabriel Felipe de Oliveira"
-print(nome[:8])
-print(nome[5:12:1]) 
-print(nome[ :: -1])   
--- dio_santander/operadoresde_associacao.py
+++
@@ -1,6 +0,0 @@
-frutas = ["limao" ,"pera", "abacaxi"]
-curso = "curso de python"
-
-print("laranja" not in frutas)
-print("python" in curso)
-print("pera" not in frutas)
