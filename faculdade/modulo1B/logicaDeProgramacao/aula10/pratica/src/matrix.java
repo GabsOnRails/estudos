6bcac916768a58f5d2b1f47548bfc624f094c666
Gabriel Notebook <gabrielfelipe.oliveira2000@gmail.com>
2025-11-03 11:05:16 -0300

    faculdade

---
+++ faculdade/modulo1B/logicaDeProgramacao/aula10/pratica/src/matrix.java
@@ -0,0 +1,20 @@
+public class matrix {
+    public static void main (String [] args){
+        int [][] matrix = {
+                {1,2,3},
+                {4,5,6},
+                {7,8,9}
+        };
+
+        //mostrar na tela
+        //
+    System.out.println("Valores matrix: ");
+      for (int i = 0; i < matrix.length; i ++){
+        for (int j = 0; j < matrix[i].length; j ++){
+           System.out.print(matrix[i][j] + " ");
+            }
+        System.out.println();
+        }
+
+    }
+}
