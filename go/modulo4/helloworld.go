6808225171843f083673c5ec8b0d3578bda889b9
¨gabriel¨ <gabrielfelipe.oliveira2000@gmail.com>
2025-11-07 16:37:29 -0300

    go inicio

---
+++ go/modulo4/helloworld.go
@@ -0,0 +1,13 @@
+// Pacote principal
+package main
+
+//fmt é para formatar
+import (
+	"fmt"
+	gabriel "strings"
+)
+
+func main () {
+	fmt.Println("Hello World!")
+	fmt.Print(gabriel.Split("gabriel","a"))
+}
