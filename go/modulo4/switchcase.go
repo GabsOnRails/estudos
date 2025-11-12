e617356a423439a8e6314ba4135d0fb352ee61ac
¨gabriel¨ <gabrielfelipe.oliveira2000@gmail.com>
2025-11-12 16:27:02 -0300

    estudos go

---
+++ go/modulo4/switchcase.go
@@ -0,0 +1,22 @@
+//go:build ignore
+
+package main
+
+import (
+	"fmt"
+)
+
+func main () {
+
+	posicao := 1
+
+	switch posicao {
+	case 1:
+		fmt.Println("Primeiro lugar!")
+	case 2: 
+	fmt.Println("Segundo lugar!")
+	case 3: 
+	fmt.Println("Terceiro lugar!")
+	}
+
+}
