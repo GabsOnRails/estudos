979d89e8246361d5a6f1619c1779507b5cb1ad66
Gabriel Notebook <gabrielfelipe.oliveira2000@gmail.com>
2025-12-23 10:30:49 -0300

    interfaces

---
+++ go/learning/interfaces.go
@@ -0,0 +1,61 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	cat := Cat{
+		color: "black",
+		paws:  4,
+		noise: "miau miau",
+	}
+
+	dog := Dog{
+		color: "grey",
+		paws:  5,
+		noise: "ruf ruf",
+	}
+	makeNoise(cat)
+	makeNoise(dog)
+}
+
+type Animal interface {
+	Noise() string     //method
+	NumberOfPaws() int //method
+
+}
+
+type Cat struct {
+	color string
+	paws  int
+	noise string
+}
+
+type Dog struct {
+	color string
+	paws  int
+	noise string
+}
+
+// cat function
+func (c Cat) Noise() string {
+	return c.noise
+}
+
+func (c Cat) NumberOfPaws() int {
+	return c.paws
+}
+
+// dog function
+func (d Dog) Noise() string {
+	return d.noise
+}
+
+func (d Dog) NumberOfPaws() int {
+	return d.paws
+}
+
+// This function will be used in all cases.
+func makeNoise(animal Animal) {
+	fmt.Println(animal.Noise())
+}
