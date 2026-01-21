d3eb0b0e90a862f8066e71dae6ef64bb8b65101e
Gabriel <gabrielfelipe.oliveira2000@gmail.com>
2026-01-21 12:55:09 -0300

    estudos go

---
+++ go/exercism/go/booking-up-for-beauty/booking_up_for_beauty.go
@@ -0,0 +1,68 @@
+package booking
+
+import (
+	"time"
+)
+
+// Schedule returns a time.Time from a string containing a date.
+func Schedule(date string) time.Time {
+	layout := "1/2/2006 15:04:05"
+
+	ts, err := time.Parse(layout, date)
+	if err != nil {
+		panic(err)
+	}
+	return ts
+}
+
+// HasPassed returns whether a date has passed.
+func HasPassed(date string) bool {
+	layout := "January 2, 2006 15:04:05"
+
+	appointment, err := time.Parse(layout, date)
+	if err != nil {
+		return false
+	}
+
+	return appointment.Before(time.Now())
+
+}
+
+// IsAfternoonAppointment returns whether a time is in the afternoon.
+func IsAfternoonAppointment(date string) bool {
+	layout := "Monday, January 2, 2006 15:04:05"
+
+	t, err := time.Parse(layout, date)
+	if err != nil {
+		return false
+	}
+
+	hour := t.Hour()
+	return hour >= 12 && hour < 18
+}
+
+// Description returns a formatted string of the appointment time.
+func Description(date string) string {
+	inputLayout := "1/2/2006 15:04:05"
+	outputLayout := "Monday, January 2, 2006, at 15:04"
+
+	t, err := time.Parse(inputLayout, date)
+	if err != nil {
+		return ""
+	}
+
+	return "You have an appointment on " + t.Format(outputLayout) + "."
+}
+
+// AnniversaryDate returns a Time with this year's anniversary.
+func AnniversaryDate() time.Time {
+	year := time.Now().UTC().Year()
+
+	return time.Date(
+		year,
+		time.September,
+		15,
+		0, 0, 0, 0,
+		time.UTC,
+	)
+}
